0a7ea9ba934f770a9d8ebaedd7ed78d537d18713
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-11-17 00:02:41 -0700

    Revert "Fix ratings reference to movies"
    This reverts commit 6dc0a6d7145c4c4fad09402846d858bb26e7be67.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,3 @@
 class Movie < ApplicationRecord
   validates_presence_of :imdbId, :title, :genres, :releaseDate, :budget
-  has_many :ratings
 end
--- app/models/rating.rb
+++
@@ -1,3 +0,0 @@
-class Rating < RatingsBase
-  validates_presence_of :userId, :movieId, :rating
-end
