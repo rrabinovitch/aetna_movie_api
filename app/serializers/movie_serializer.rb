4db588d76ca625b7434e7eae68718db448111871
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-11-16 17:59:02 -0700

    Format genres

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,4 +1,9 @@
 class MovieSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :imdbId, :title, :genres, :releaseDate, :budget
+
+  attributes :imdbId, :title, :releaseDate, :budget
+
+  attribute :genres do |movie|
+    JSON.parse(movie.genres)
+  end
 end
