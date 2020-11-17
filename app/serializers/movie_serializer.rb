a5949b4bbd9e487a8025434215c5eeaa19cf7333
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-11-16 18:51:24 -0700

    Temp fix for genres parsing issue

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,11 +1,11 @@
 class MovieSerializer
   include FastJsonapi::ObjectSerializer
-
+  binding.pry
   attributes :imdbId, :title, :releaseDate, :budget
 
-  attribute :genres do |movie|
-    JSON.parse(movie.genres)
-  end
+  attribute :genres #do |movie|
+    # JSON.parse(movie.genres)
+#  end
 
   attribute :budget do |movie|
     "$#{movie.budget}.00"
