de110d043018878bfc632a86ac963acab5b755a7
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-11-16 21:16:21 -0700

    Revert "Update movies factory"
    This reverts commit c774e7b2652db7091d374063be5078a2d17834d3.

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -2,12 +2,9 @@ FactoryBot.define do
   factory :movie do
     imdbId { "tt0094675" }
     title { "Movie Title" }
-    overview { "This is the movie description." }
-    productionCompanies { [{name: "Production Co.", "id": 111}, {"name": "Production Group Inc.", "id": 222}] }
     releaseDate { "1988-10-21" }
     budget { 1000000 }
     runtime { 105 }
-    language { "English" }
     genres { [{id: 18, name: "Drama"}, {id: 80, name: "Crime"}] }
   end
 end
