91739c478c2218ac1e7e44463f127f4f5271f17f
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-11-16 19:40:36 -0700

    Create movies#show action

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -4,4 +4,9 @@ class Api::V1::MoviesController < ApplicationController
     movies = Movie.all.paginate(page: page, per_page: 50)
     render json: MovieSerializer.new(movies)
   end
+
+  def show
+    movie = Movie.find(params[:id])
+    render json: MovieSerializer.new(movie)
+  end
 end
